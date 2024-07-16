0b6a151d5373999d407c3446464878413376f5f8
Sriram <kinjarapusriram2000@gmail.com>
2024-07-17 02:55:00 +0530

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings.js';
-import { ExampleHomebridgePlatform } from './platform.js';
+import { PicoPirPlatform } from './picoPirPlatform.js';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, PicoPirPlatform);
 };
